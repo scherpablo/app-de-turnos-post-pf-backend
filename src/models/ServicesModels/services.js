606d6e35667c13f07976f1139adfecfa09a8a0a8
Simply92 <pedrouner92@gmail.com>
2024-02-09 16:10:05 -0300

    modelos

---
+++ src/models/ServicesModels/services.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("Service", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    service_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    service_price: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    service_detail: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    isDelete: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false,
+    },
+  });
+};
---
+++ src/models/UserModels/userLogin.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "UserLogin",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        isEmail: true,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
